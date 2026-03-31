0f1a792ac465db3926150b9a31f6d657bd6b550f
Victor-PC <filpsixi@mail.ru>
2026-03-31 18:49:33 +0300

    Revert "rm test_proj from master"
    This reverts commit d28febc51f1d8494fc515f61d9ee5034ca9b257a.
    
    	new file:   two_m/__init__.py
    	new file:   two_m/main.py
    	new file:   two_m/models.py
    	new file:   two_m/procedures.py
    	new file:   two_m/settings.env
    	new file:   two_m/tests.py

---
+++ two_m/main.py
@@ -0,0 +1,37 @@
+"""
+Copyright (C) 2025 Литовченко Виктор Иванович (filthps)
+"""
+import os
+import typing
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), "settings.env"))
+DATABASE_PATH = os.environ.get("DATABASE_PATH")
+MEMCACHE_PATH = os.environ.get("CACHE_PATH")
+# Далее константы классов
+# Tool
+RELEASE_INTERVAL_SECONDS: float = 20.0
+CACHE_LIFETIME_HOURS: int = 1 * 60 * 60
+RETRYING_CLIENT_ATTEMPTS: int = 10
+RETRYING_CLIENT_RETRY_DELAY: float = 0.3
+# NodeDataManager
+IGNORE_NODE_PRIMARY_KEY_ERROR: bool = False
+INCOMING_DATA_VALIDATION_LEVEL: typing.Literal["filter", "strong"] = "strong"
+# SQLAlchemyQueryManager
+MAX_RETRIES: typing.Union[int, typing.Literal["no-limit"]] = "no-limit"
+# ResultORMCollection
+ADD_TABLE_NAME_PREFIX: typing.Literal["auto", "add", "no-prefix"] = "auto"
+# PointerCacheTools
+WRAP_ITEM_MAX_LENGTH = 30
+# ConnectionManager
+DATABASE_CONNECTION_ALIVE_SEC = 3
+CACHE_CONNECTION_ALIVE_SEC = 3
+# ResultPaginatorMixin
+ITEMS_ON_PAGE = float("inf")
+# OrderByMixin
+BY_PRIMARY_KEY = True
+BY_COLUMN_NAME = None
+BY_CREATE_TIME = False
+BY_ALPHABET = True
+BY_STRING_LENGTH = False
+REVERSED = False
