0050f802cb483961e4045bbc1fc5fc9580a948b0
Victor-PC <filpsixi@mail.ru>
2026-05-23 15:37:27 +0300

    Константы

--- two_m_root/templates/main.py
+++ two_m_root/templates/main.py
@@ -9,26 +9,34 @@ load_dotenv(os.path.join(os.path.dirname(__file__), "settings.env"))
 DATABASE_PATH = os.environ.get("DATABASE_PATH")
 MEMCACHE_PATH = os.environ.get("CACHE_PATH")
 BASE_DIR_ROOT = os.path.dirname(__file__)
+
 # Далее константы классов
 # Tool
 RELEASE_INTERVAL_SECONDS: float = 20.0
 CACHE_LIFETIME_HOURS: int = 1 * 60 * 60
 RETRYING_CLIENT_ATTEMPTS: int = 10
 RETRYING_CLIENT_RETRY_DELAY: float = 0.3
+
 # NodeDataManager
 IGNORE_NODE_PRIMARY_KEY_ERROR: bool = False
 INCOMING_DATA_VALIDATION_LEVEL: typing.Literal["filter", "strong"] = "strong"
+
 # SQLAlchemyQueryManager
 MAX_RETRIES: typing.Union[int, typing.Literal["no-limit"]] = "no-limit"
+
 # ResultORMCollection
 ADD_TABLE_NAME_PREFIX: typing.Literal["auto", "add", "no-prefix"] = "auto"
+
 # PointerCacheTools
 WRAP_ITEM_MAX_LENGTH = 30
+
 # ConnectionManager
 DATABASE_CONNECTION_ALIVE_SEC = 3
 CACHE_CONNECTION_ALIVE_SEC = 3
+
 # ResultPaginatorMixin
 ITEMS_ON_PAGE = float("inf")
+
 # OrderByMixin
 BY_PRIMARY_KEY = True
 BY_COLUMN_NAME = None
@@ -36,3 +44,8 @@ BY_CREATE_TIME = False
 BY_ALPHABET = True
 BY_STRING_LENGTH = False
 REVERSED = False
+
+#########
+# DEBUG #
+#########
+ITER_ONLY_VISIBLE_ITEMS_AS_DEFAULT = True  # Скрывать или не скрывать скрытые ноды из итерируемой последовательности
